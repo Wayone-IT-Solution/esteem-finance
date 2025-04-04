9da1d683d6abed7ffad2886d6815807808aef3e9
coolboyalan <deepaky954000@gmail.com>
2025-04-04 17:20:10 +0530

    fixed env error

--- src/services/loanApplication.service.js
+++ src/services/loanApplication.service.js
@@ -1,17 +1,20 @@
 import LoanApplication from "#models/loanApplication";
 import api from "../utils/sms.util.cjs";
 import httpStatus from "http-status";
+import env from "#configs/env";
 import Service from "#services/base";
 
 class LoanApplicationService extends Service {
   static Model = LoanApplication;
 
   static async create(data) {
+    const code = Math.floor(100000 + Math.random() * 900000);
+    data.otp = code;
     const doc = await this.Model.create(data);
 
     var smsMessage = new api.SmsMessage();
 
-    var smsApi = new api.SMSApi("", "7B6586BD-AB79-CC69-2DA3-69B8600BF66A");
+    var smsApi = new api.SMSApi(env.CLICKSEND_USER, env.CLICKSEND_API);
 
     smsMessage.to = doc.countryCode + doc.mobile;
     smsMessage.body = `Your One-Time Password (OTP) for verification is: ${doc.otp} This code is valid for the next 10 minutes. For your security, please do not share this code with anyone.`;
